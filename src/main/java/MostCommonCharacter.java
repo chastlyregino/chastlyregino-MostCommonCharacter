f688b73a5b80455edf1a54bf7bbd2490eaefc619
Finn Chastly Manapat <chastly_regino@icloud.com>
2024-07-29 18:26:21 +0000

    returning the letter in the string that is most common

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.util.Map.Entry;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +10,30 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        char mostCommonChar = 0;
+        Map<Character, Integer> charMap = new HashMap<>();
+        char[] charArray = str.toCharArray(); // converting string into char[]
+
+        for ( char letter : charArray ) { // to count the occurrances of the character in a string
+            Integer value = charMap.get(letter);
+
+            if ( value == null )
+                charMap.put(letter, 1);
+ 
+            else {
+
+                charMap.put(letter, value + 1);
+            }
+        }
+
+        int maxValueInMap = (Collections.max(charMap.values())); //getting the largest value available in the map
+
+        for( Entry<Character, Integer> setPair : charMap.entrySet() ) { // looping through the map to get the key pair of the value
+            if( setPair.getValue() == maxValueInMap ) {
+                mostCommonChar = setPair.getKey();
+            }
+        }
+
+        return mostCommonChar;
     }
 }
